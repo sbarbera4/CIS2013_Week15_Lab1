81042c9a016d25047420bb8660ae48a3183eb6e7
stephen <stevie.barbera@gmail.com>
2018-05-02 19:42:28 -0500

    Initial commit

---
+++ Animal.cpp
@@ -0,0 +1,56 @@
+#include "Animal.h"
+
+using namespace std;
+
+Animal::Animal() : number_of_legs(4), is_alive(true), species("Cat")
+{
+	/*
+	--- same as below ---
+	Animal::Animal()
+	{
+		set_legs(4);
+		set_alive(true);
+		set_species("Cat");
+	}
+	*/
+}
+
+
+Animal::Animal(int legs, bool alive, string spec)
+{
+number_of_legs = legs;
+is_alive = alive;
+species = spec;
+}
+
+Animal::~Animal()
+{
+
+}
+
+void Animal::set_legs(int legs)
+{
+	number_of_legs = legs;
+}
+int Animal::get_legs()
+{
+	return number_of_legs;
+}
+
+void Animal::set_alive(bool alive)
+{
+	is_alive = alive;
+}
+bool Animal::get_alive()
+{
+	return is_alive;
+}
+
+void Animal::set_species(string spec)
+{
+	species = spec;
+}
+string Animal::get_species()
+{
+	return species;
+}
---
+++ Animal.h
@@ -0,0 +1,27 @@
+#include <string>
+
+using namespace std;
+
+class Animal
+{
+private:
+	int legs;
+	bool living;
+	string species;
+
+public:
+
+	Animal();
+	Animal(int legs, bool alive, string species)
+	~Animal();
+
+	void set_legs(int);
+	int get_legs();
+	
+	void set_alive(bool);
+	bool get_alive();
+
+	void set_species(string);
+	string get_species();
+
+};
